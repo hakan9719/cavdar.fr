3e2c202fbb9b744eb29334a8fc06ec207b5bad36
Hakan <hakan9719@gmail.com>
2021-11-21 17:04:04 +0100

    change email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ export default function Contact() {
   const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: "test@example.com", // Change to your recipient
-    from: "test@example.com", // Change to your verified sender
+    to: "contact@cavdar.fr", // Change to your recipient
+    from: "contact@cavdar.fr", // Change to your verified sender
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
