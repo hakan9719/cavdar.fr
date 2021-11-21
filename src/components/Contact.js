1abe9b8d1e270ce7b4146b6b2b8037f890204063
Hakan <hakan9719@gmail.com>
2021-11-21 17:30:38 +0100

    try sendgrid

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,15 @@
 // import React from "react";
 // import { Col, Container, Form, Row } from "react-bootstrap";
-
+// require('dotenv').config();
+const dotenv = require('dotenv');
 export default function Contact() {
   // using Twilio SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
   const sgMail = require("@sendgrid/mail");
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  console.log(process.env);
+  console.log(dotenv);
+  console.log(dotenv.config());
+  sgMail.setApiKey('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');
   const msg = {
     to: "contact@cavdar.fr", // Change to your recipient
     from: "contact@cavdar.fr", // Change to your verified sender
