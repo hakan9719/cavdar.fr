a06da00a1a92883e265b2dc75d11680a8f349382
Hakan <hakan9719@gmail.com>
2021-12-04 19:01:16 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,64 @@
-import emailjs from 'emailjs-com'
-import { Col, Container, Form, Row } from 'react-bootstrap'
-import { init } from 'emailjs-com';
+import React, { useState } from "react";
+import emailjs, { init } from "emailjs-com";
+import {Col, Container, Form, Row} from "react-bootstrap";
 
 export default function Contact() {
   
-  const sendEmail = e => {
-    init(process.env.REACT_APP_USER_ID);
-    e.preventDefault()
-    emailjs.sendForm(process.env.REACT_APP_SERVICE, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_USER_ID)
-    .then(
-      result => {
-        console.log(result.text)
-      },
-      error => {
-        console.log(error.text)
-      }
-      )
-    }
+  const [inputs, setInputs] = useState(false);
 
-    return (
-      <Container className="text-center my-5" id="contact">
-            <h1>Contact</h1>
-            <form onSubmit={sendEmail}>
-                <Row xs={1} md={3} lg={12} className="mb-3 w-75 mx-auto">
-                    <Form.Group as={Col} controlId="name">
-                        <Form.Control type="text" name="name" placeholder="Nom" />
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="email">
-                        <Form.Control type="email" name="email" placeholder="Email" />
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="message">
-                        <Form.Control type="textarea" name="message" placeholder="Votre Message" />
-                    </Form.Group>
-                </Row>
-                <button className="btn btn-primary" type="submit">Envoyer</button>
-            </form>
-        </Container>
-    );
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+    console.log(setInputs);
+    setInputs("");
+    console.log(setInputs);
+    sendEmail(event);
+  }
+
+  const sendEmail =(event)=>{
+    init(process.env.REACT_APP_USER_ID);
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE,
+        process.env.REACT_APP_TEMPLATE_ID,
+        event.target,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+
+  return (
+    <Container className="text-center my-5" id="contact">
+      <h1>Contact</h1>
+      <form onSubmit={handleSubmit}>
+        <Row xs={1} md={3} lg={12} className="mb-3 w-75 mx-auto">
+          <Form.Group as={Col} controlId="name" className="mt-2">
+            <Form.Control type="text" name="name" placeholder="Nom" onChange={handleChange}/>
+          </Form.Group>
+          <Form.Group as={Col} controlId="email" className="mt-2">
+            <Form.Control type="email" name="email" placeholder="Email" onChange={handleChange}/>
+          </Form.Group>
+          <Form.Group as={Col} controlId="message" className="mt-2">
+            <Form.Control type="textarea" name="message" placeholder="Votre Message" onChange={handleChange}/>
+          </Form.Group>
+        </Row>
+        <button className="btn btn-primary" type="submit">
+          Envoyer
+        </button>
+      </form>
+    </Container>
+  );
 }
